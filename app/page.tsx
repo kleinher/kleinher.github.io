7df7016384bfa21646ca84b182fe3d83de07ddc2
Hernan Kleinubing <hernan.k97@gmail.com>
2024-11-14 21:24:55 +0100

    Added blog post

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Hernan Kleinubing
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm a Software Engineer focused mainly in Backend. Also Electronics Hobbist 
+         interested in technology and it's apliances. Driven by clean code principles, 
+         scalability, and optimizing performance to build reliable and high-performing 
+         applications.
+         Currently based in Spain`}
       </p>
       <div className="my-8">
         <BlogPosts />
